d59cbdd2c4326df1b8fc2715c6202d854028b01d
Edward Woolhouse <danishcake@hotmail.com>
2022-06-07 07:26:16 +0100

    Remove unused test configuration

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/tests/**/test_*.ts'],
-  setupFilesAfterEnv: ['jest-extended/all']
-};
